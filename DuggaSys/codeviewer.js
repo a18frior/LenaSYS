b05c12a0eb77c811747c775cfff6fddd41867ed5
a17okaoz <a17okaoz@student.his.se>
2019-04-17 15:05:16 +0200

    Update codeviewer.js

--- DuggaSys/codeviewer.js
+++ DuggaSys/codeviewer.js
@@ -1775,6 +1775,7 @@ function tbl(numberofboxes) {
   `;
 
   document.getElementById("tableview").innerHTML=tblstr;
+  document.getElementById("tableview").innerHTML="hej";
 }
 
 //-----------------------------------------------------------------------------
