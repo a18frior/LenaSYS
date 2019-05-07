ee50aea38190ed22bf7a3d69a787e25885700ea3
Oscar <a17oscno@student.his.se>
2019-05-07 14:29:36 +0100

    logging and debugging some more

--- DuggaSys/sectioned.js
+++ DuggaSys/sectioned.js
@@ -342,7 +342,7 @@ function prepareItem() {
   param.grptype = $("#grptype").val();
   param.deadline = $("#setDeadlineValue").val()+" "+$("#deadlinehours").val()+":"+$("#deadlineminutes").val();
 
-  console.log(param.lid);
+  console.log(param.deadline);
 
   return param;
 }
