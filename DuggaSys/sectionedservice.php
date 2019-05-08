7f3f038609b60e0715f4ee9a6794f9a75bc254fc
Oscar <a17oscno@student.his.se>
2019-05-08 09:57:48 +0100

    testing

--- DuggaSys/sectionedservice.php
+++ DuggaSys/sectionedservice.php
@@ -303,6 +303,7 @@ if($gradesys=="UNK") $gradesys=0;
 						}
 					}
 				}else if(strcmp($opt,"UPDATEDEADLINE")===0){
+					var_dump($opt);
 					// $deadlinequery = $pdo->prepare("UPDATE quiz SET deadline=:deadline WHERE id=:link;");
 					// $deadlinequery->bindParam(':deadline',$deadline);
 					// $deadlinequery->bindParam(':link',$link)
