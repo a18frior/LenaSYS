16b9fadd7d286f48abefdb570ce51f9a571fb58b
Oscar <a17oscno@student.his.se>
2019-05-08 12:28:10 +0100

    testing

--- DuggaSys/sectionedservice.php
+++ DuggaSys/sectionedservice.php
@@ -303,16 +303,14 @@ if($gradesys=="UNK") $gradesys=0;
 						}
 					}
 				}else if(strcmp($opt,"UPDATEDEADLINE")===0){
-					//var_dump(strcmp($opt);
-					// $deadlinequery = $pdo->prepare("UPDATE quiz SET deadline=:deadline WHERE id=:link;");
-					// $deadlinequery->bindParam(':deadline',$deadline);
-					// $deadlinequery->bindParam(':link',$link)
-					
-					// if(!$deadlinequery->execute()){
-					// 	$error=$deadlinequery->errorInfo();
-					// 	$debug="ERROR THE DEADLINE QUERY FAILED".$error[2];
-					// }
+					$deadlinequery = $pdo->prepare("UPDATE quiz SET deadline=:deadline WHERE id=:link;");
+					$deadlinequery->bindParam(':deadline',$deadline);
+					$deadlinequery->bindParam(':link',$link)
 
+					if(!$deadlinequery->execute()){
+						$error=$deadlinequery->errorInfo();
+						$debug="ERROR THE DEADLINE QUERY FAILED".$error[2];
+					}
 				}else if(strcmp($opt,"UPDATEVRS")===0) {
 						$query = $pdo->prepare("UPDATE vers SET versname=:versname,startdate=:startdate,enddate=:enddate WHERE cid=:cid AND coursecode=:coursecode AND vers=:vers;");
 						$query->bindParam(':cid', $courseid);
